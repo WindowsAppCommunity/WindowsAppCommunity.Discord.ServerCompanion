c7c8672bbde6bbd537451ad06d24fcacdcd38ad7
Arlo Godfrey <arlo.godfrey@outlook.com>
2024-01-12 17:31:29 -0600

    Use test fixture and mocks in tests

--- tests/WinAppCommunity.Discord.ServerCompanion.Tests/UserCommandsTests.cs
+++ tests/WinAppCommunity.Discord.ServerCompanion.Tests/UserCommandsTests.cs
@@ -2,24 +2,27 @@ using Remora.Discord.Commands.Contexts;
 using Remora.Discord.Commands.Feedback.Services;
 using WinAppCommunity.Discord.ServerCompanion.Commands;
 using WinAppCommunity.Discord.ServerCompanion.Keystore;
+using WinAppCommunity.Discord.ServerCompanion.Tests.Mocks;
 
 namespace WinAppCommunity.Discord.ServerCompanion.Tests;
 
 [TestClass]
-public class UserCommandsTests
+public partial class UserCommandsTests
 {
     [TestMethod]
     public async Task RegisterUser()
     {
-        Assert.IsNotNull(TestFixture.Client);
+        var workingFolder = await TestFixture.SafeCreateWorkingFolder($"{nameof(UserCommandsTests)}.{nameof(RegisterUser)}");
         
-        IInteractionContext context = null!;
-        IFeedbackService feedback = null!;
-        UserKeystore keystore = null!;
+        UserKeystore keystore = new UserKeystore(workingFolder);
+        IInteractionContext context = new MockInteractionContext();
+        IFeedbackService feedback = new MockFeedbackService();
         
         var userCommands = new UserCommands(context, feedback, keystore, TestFixture.Client);
         
-        
+        var result = await userCommands.RegisterUserAsync(name: "userA", contactEmail: "test.ing@example.com");
+
+        Assert.IsTrue(result.IsSuccess);
     }
         
     [TestMethod]
