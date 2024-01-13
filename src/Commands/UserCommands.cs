7e24e282a3b57621fe4e404bb7ace47a448ed303
Arlo Godfrey <arlo.godfrey@outlook.com>
2024-01-12 22:59:35 -0600

    Better error propogation, fixed email being added as (invalid) Url

--- src/Commands/UserCommands.cs
+++ src/Commands/UserCommands.cs
@@ -4,11 +4,13 @@ using Ipfs.Http;
 using OwlCore.Kubo;
 using Remora.Commands.Attributes;
 using Remora.Commands.Groups;
+using Remora.Discord.API.Abstractions.Objects;
 using Remora.Discord.Commands.Contexts;
 using Remora.Discord.Commands.Feedback.Services;
 using Remora.Discord.Extensions.Embeds;
 using Remora.Results;
 using System.ComponentModel;
+using WinAppCommunity.Discord.ServerCompanion.Commands.Errors;
 using WinAppCommunity.Discord.ServerCompanion.Keystore;
 using WinAppCommunity.Sdk;
 using WinAppCommunity.Sdk.Models;
@@ -57,7 +59,7 @@ public class UserCommands : CommandGroup
                 connections.Add(new EmailConnection(contactEmail));
 
             // Create user
-            var user = new User(name, connections);
+            var user = new User(name, connections.ToArray());
 
             // Get CID of new user object
             var cid = await user.GetCidAsync(_client, CancellationToken.None);
@@ -65,13 +67,13 @@ public class UserCommands : CommandGroup
             // Create ipns address
             // Use name "temp" to create key
             var key = await _client.Key.CreateAsync(name: "temp", "ed25519", 4096);
-            
+
             // Rename key name to the key id
             var renamed = await _client.Key.RenameAsync("temp", $"{key.Id}");
-            
+
             // Publish data to ipns
             await _client.Name.PublishAsync(cid, $"{renamed.Id}");
-            
+
             var peerCid = new Cid { Hash = key.Id };
 
             // Save new renamed
@@ -79,11 +81,11 @@ public class UserCommands : CommandGroup
             await _userKeystore.SaveAsync();
 
             return (Result)await _feedbackService.SendContextualInfoAsync("Ran to completion");
-
         }
         catch (Exception ex)
         {
-            return (Result)await _feedbackService.SendContextualErrorAsync($"An error occurred:\n\n{ex}");
+            await _feedbackService.SendContextualErrorAsync($"An error occurred:\n\n{ex}");
+            return (Result)new UnhandledExceptionError(ex);
         }
     }
 
@@ -91,28 +93,46 @@ public class UserCommands : CommandGroup
     [Description("Displays your user profile")]
     public async Task<IResult> GetProfileAsync()
     {
-        var discordUser = _context.Interaction.Member.Value.User;
-        var discordId = discordUser.Value.ID;
-
-        var managedUser = _userKeystore.ManagedUsers.FirstOrDefault(x =>
-            x.User.Connections.Any(o => o is DiscordConnection discordConnection && discordConnection.discordId == $"{discordId}"));
-
-        if (managedUser is null)
+        try
         {
-            return (Result)await _feedbackService.SendContextualErrorAsync("You're not registered");
+            var discordUser = _context.Interaction.Member.Value.User;
+            var discordId = discordUser.Value.ID;
+
+            var managedUser = _userKeystore.ManagedUsers.FirstOrDefault(x => x.User.Connections.Any(o => o is DiscordConnection discordConnection && discordConnection.DiscordId == $"{discordId}"));
+            if (managedUser is null)
+                return (Result)new UserProfileNotFoundError();
+
+            managedUser.User = await managedUser.IpnsCid.ResolveIpnsDagAsync<User>(_client, CancellationToken.None);
+
+            Guard.IsNotNullOrWhiteSpace(managedUser.User.Name);
+
+            var embedBuilder = new EmbedBuilder()
+                .WithAuthor(managedUser.User.Name);
+
+            if (!string.IsNullOrWhiteSpace(managedUser.User.MarkdownAboutMe))
+                embedBuilder = embedBuilder.WithDescription(managedUser.User.MarkdownAboutMe);
+
+            var emailConnection = managedUser.User.Connections.FirstOrDefault(x => x is EmailConnection emailConnection) as EmailConnection;
+            if (emailConnection is not null)
+            {
+                var embedWithFieldResult = embedBuilder.AddField("email", emailConnection.Email, inline: true);
+                if (!embedWithFieldResult.IsSuccess)
+                    return embedWithFieldResult;
+
+                embedBuilder = embedWithFieldResult.Entity;
+            }
+
+            var embedBuildResult = embedBuilder.Build();
+            if (!embedBuildResult.IsSuccess)
+                return embedBuildResult;
+
+            return await _feedbackService.SendContextualEmbedAsync(embedBuildResult.Entity);
         }
-
-        managedUser.User = await managedUser.IpnsCid.ResolveIpnsDagAsync<User>(_client, CancellationToken.None);
-
-        Guard.IsNotNullOrWhiteSpace(managedUser.User.Name);
-
-        var embedBuilder = new EmbedBuilder()
-            .WithAuthor(managedUser.User.Name);
-
-        if (!string.IsNullOrWhiteSpace(managedUser.User.MarkdownAboutMe))
-            embedBuilder = embedBuilder.WithDescription(managedUser.User.MarkdownAboutMe);
-
-        return (Result)await _feedbackService.SendContextualEmbedAsync(embedBuilder.Build().Entity);
+        catch (Exception ex)
+        {
+            await _feedbackService.SendContextualErrorAsync($"An error occurred:\n\n{ex}");
+            return (Result)new UnhandledExceptionError(ex);
+        }
     }
 }
 
