54acafea1b5c82afeffc393b81baa84d1f72ef92
Arlo Godfrey <arlo.godfrey@outlook.com>
2024-01-12 22:54:38 -0600

    Add new error types

---
+++ src/Commands/Errors/UnhandledExceptionError.cs
@@ -0,0 +1,5 @@
+﻿using Remora.Results;
+
+namespace WinAppCommunity.Discord.ServerCompanion.Commands.Errors;
+
+public record UnhandledExceptionError(Exception ex) : ResultError($"{ex}");
---
+++ src/Commands/Errors/UserProfileNotFoundError.cs
@@ -0,0 +1,8 @@
+﻿using Remora.Results;
+
+namespace WinAppCommunity.Discord.ServerCompanion.Commands.Errors;
+
+/// <summary>
+/// An error that occurs when a user profile can't be found.
+/// </summary>
+public record UserProfileNotFoundError() : ResultError("The user wasn't found and may not be registered");
